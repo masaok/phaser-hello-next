7666c8ab5ef7e2efd32cbc744b4f199c57b9e907
Masao Kitamura <masaok@gmail.com>
2024-07-19 05:39:35 -0700

    Fixing Player types

--- src/app/by-example/runner/gameobjects/player.ts
+++ src/app/by-example/runner/gameobjects/player.ts
@@ -1,10 +1,12 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
+  // class Player extends Phaser.GameObjects.Rectangle {
   jumping: boolean
   invincible: boolean
   health: number
 
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string | number) {
-    super(scene, x, y, texture, frame)
+  constructor(scene: Phaser.Scene, x: number, y: number) {
+    // super(scene, x, y, texture, frame)
+    super(scene, x, y, 'player')
     this.setOrigin(0.5)
     scene.add.existing(this)
     scene.physics.add.existing(this)
