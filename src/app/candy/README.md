62616368ed47fdc8e42e752bfc13bd2edf10c255
Masao Kitamura <masaok@gmail.com>
2024-07-20 14:54:49 -0700

    Preparing Candy clone and transitioning code to TS

---
+++ src/app/candy/README.md
@@ -0,0 +1,5 @@
+- [FAQ](#faq)
+
+### FAQ
+
+- Game Music? `splash.js > startGame() > playGameMusic()`
---
+++ src/app/candy/TheGame.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+// https://pello.itch.io/pushpull
+
+import { useEffect } from 'react'
+
+import * as Phaser from 'phaser'
+
+import Bootloader from './scenes/bootloader'
+import Splash from './scenes/splash'
+import Transition from './scenes/transition'
+import Game from './scenes/game'
+// import Outro from './scenes/outro'
+
+const TheGame: React.FC = () => {
+  useEffect(() => {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+      },
+      backgroundColor: '#0072bc',
+      parent: 'phaser-container', // must match div id
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0, x: 0 },
+          debug: true,
+        },
+      },
+      scene: [Bootloader, Splash, Transition, Game],
+    }
+
+    const game = new Phaser.Game(config)
+
+    return () => {
+      game.destroy(true)
+    }
+  }, [])
+
+  return <div id="phaser-container" style={{ width: '100vw', height: '100vh' }} />
+}
+
+export default TheGame
