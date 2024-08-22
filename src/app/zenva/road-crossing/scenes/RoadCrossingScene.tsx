e48c30a5ba3d5c2ba0d1ed7eebaa8a1e45f419ec
Masao Kitamura <masaok@gmail.com>
2024-08-21 17:58:55 -0700

    Scaling and Flipping done

--- src/app/zenva/road-crossing/scenes/RoadCrossingScene.tsx
+++ src/app/zenva/road-crossing/scenes/RoadCrossingScene.tsx
@@ -7,8 +7,12 @@ class RoadCrossingScene extends Scene {
   }
 
   preload() {
+    // 02 - Rendering Sprites
     this.load.image('background', '/zenva/assets/background.png')
     this.load.image('player', '/zenva/assets/player.png')
+
+    // 03 - Scaling and Flipping
+    this.load.image('enemy', 'assets/dragon.png')
   }
 
   create() {
@@ -36,13 +40,51 @@ class RoadCrossingScene extends Scene {
     // Center the sprite in the viewport
     bg.setPosition(viewportWidth / 2, viewportHeight / 2)
 
-    // let gameW = this.sys.game.config.width
-    // let gameH = this.sys.game.config.height
+    let gameW = this.sys.game.config.width
+    let gameH = this.sys.game.config.height
 
-    // console.log(gameW, gameH)
+    console.log(gameW, gameH)
 
-    // console.log(bg)
-    // console.log(this)
+    console.log(bg)
+    console.log(this)
+
+    // 03 - Scaling and Flipping
+    // change the origin to the top-left corner (nope, shifts the bg to the bottom right)
+    // bg.setOrigin(0, 0)
+
+    // create the player
+    let player = this.add.sprite(70, 180, 'player')
+
+    // set the origin of the player to its center
+    player.setOrigin(0.5, 0.5)
+
+    // we are reducing the width by 50%, and we are doubling the height
+    player.setScale(0.5)
+
+    // ensure the player is within the visible area
+    player.setPosition(viewportWidth / 2, viewportHeight / 2)
+
+    // 03 - Scaling and Flipping
+    // create an enemy
+    let enemy1 = this.add.sprite(250, 180, 'enemy')
+
+    // set the origin of the enemy to its center
+    enemy1.setOrigin(0.5, 0.5)
+
+    // ensure the enemy is within the visible area
+    enemy1.setPosition(viewportWidth / 2 + 100, viewportHeight / 2)
+
+    // set the depth of the enemy to ensure it is rendered on top of other elements
+    enemy1.setDepth(1)
+
+    // create a second enemy
+    let enemy2 = this.add.sprite(450, 180, 'enemy')
+    enemy2.setPosition(viewportWidth / 2 + 200, viewportHeight / 2)
+    // enemy2.displayWidth = 300
+
+    // flip
+    enemy1.flipX = true
+    enemy2.flipX = true
   }
 }
 
